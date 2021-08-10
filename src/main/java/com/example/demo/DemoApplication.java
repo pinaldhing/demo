c08a9d2d4f1da979e9af0a30a627cfa3dbd35dc5
pinal <pinaldhing@gmail.com>
2021-08-10 14:33:39 +0530

    added pinal age

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		System.out.println("my age is 19");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 

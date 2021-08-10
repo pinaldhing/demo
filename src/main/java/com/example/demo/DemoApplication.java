2966d098ea7629d3668f0e4240bd0b9a5234599c
pinal <pinaldhing@gmail.com>
2021-08-10 15:10:31 +0530

    flipkart dev done

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,7 @@ public class DemoApplication {
 		System.out.println("my name is bhavya");
 		System.out.println("my age is 21");
 		System.out.println("flipkart_auth");
+		System.out.println("flipkart dev done");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 

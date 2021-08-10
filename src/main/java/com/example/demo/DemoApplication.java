844a11f781a4a1a097c4d020a4edf81a844285cc
pinal <pinaldhing@gmail.com>
2021-08-10 14:32:51 +0530

    added pinal name

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		System.out.println("my name is pinal");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 

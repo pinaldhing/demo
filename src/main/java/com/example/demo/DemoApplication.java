0485d548399fa55199f1eed8d09b3a5ec1653753
pinal <pinaldhing@gmail.com>
2021-08-10 18:41:13 +0530

    all sout removed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,15 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello world!");
-		System.out.println("my name is pinal");
-		System.out.println("my age is 19");
-		System.out.println("my name is bhavya");
-		System.out.println("my age is 21");
-		System.out.println("flipkart_auth");
-		System.out.println("flipkart dev done");
-		System.out.println("escrow_account_settlement");
-		System.out.println("escrow dev done");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
